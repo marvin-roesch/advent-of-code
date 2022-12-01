585a04c1b2558c052c9550e94611c2506b3a2ccc
Marvin Rösch <marvinroesch99@gmail.com>
2022-12-01 14:28:55 +0100

    2022: Clean up Day 1 Nim a little

--- 2022/day1.nim
+++ 2022/day1.nim
@@ -4,14 +4,14 @@ import std/strformat
 import std/sequtils
 import std/sugar
 
-let input = readFile("day1.txt")
-let parsed = collect:
-    for group in input.split("\n\n"):
-        collect:
-            for line in group.split("\n"):
-                if line.len() > 0:
+let
+    input = readFile("day1.txt")
+    parsed = collect:
+        for group in input.split("\n\n"):
+            collect:
+                for line in group.strip().split("\n"):
                     parseInt(line)
-let sums = parsed.mapIt(it.foldl(a + b))
+    sums = parsed.mapIt(it.foldl(a + b))
 
 echo fmt"Part 1: {sums.max()}"
 echo fmt"Part 2: {sums.sorted(SortOrder.Descending)[0..2].foldl(a + b)}"
